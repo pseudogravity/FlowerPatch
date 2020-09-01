73d0c8f8fabf487db87ecc13bc9ef4cfaf0821b0
pseudogravity <68665732+pseudogravity@users.noreply.github.com>
2020-08-31 20:53:43 -0400

    update example

--- src/Replicate.java
+++ src/Replicate.java
@@ -2,16 +2,16 @@ import java.util.Random;
 
 public class Replicate {
   public static void main(String[] args) {
-    long DFZ = 539778;
-    int[] center = { 0, 0, 0 };
+    long DFZ = 3792043554L; // produces flowers at (0,0,0) (1,1,1) (2,2,2) (3,3,3)
+    int[] center = { 7, 3, 7 };
     // int[] center = { 99, 68, -100 }; // for converting to absolute coords
 
     long internalseed = LCG.JAVA.combine(DFZ).nextSeed(0);
     Random random = new Random(internalseed ^ 0x5DEECE66DL);
     for (int i = 0; i < 64; i++) {
-      int i1 = (7 + random.nextInt(8)) - random.nextInt(8);
-      int j1 = (3 + random.nextInt(4)) - random.nextInt(4);
-      int k1 = (7 + random.nextInt(8)) - random.nextInt(8);
+      int i1 = (random.nextInt(8)) - random.nextInt(8);
+      int j1 = (random.nextInt(4)) - random.nextInt(4);
+      int k1 = (random.nextInt(8)) - random.nextInt(8);
       System.out.printf("%2d\t%2d\t%2d%n", i1 + center[0], j1 + center[1], k1 + center[2]);
     }
   }
